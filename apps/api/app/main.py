a8c9b6baf84cf0bdac86ed79f930c5cef42ccce3
Simon Aguilera <saguilera1608@gmail.com>
2025-10-19 19:30:18 -0300

    fix api

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -3,11 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import routes as v1_routes
 from app.db.session import SessionLocal
 from app.db.init_db import init_db
-import os
 
-# Only run init_db if not in a test environment
-if os.environ.get("TESTING") != "True":
-    init_db(db=SessionLocal())
+init_db(db=SessionLocal())
 
 app = FastAPI()
 
